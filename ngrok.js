5ca259ce86880055a08b3e4b36b03747ac126ed2
Nick Williams <git@nickawilliams.com>
2018-02-22 14:13:45 -0800

    Fixed some linting issues.

--- ngrok.js
+++ ngrok.js
@@ -7,11 +7,16 @@ const exec = require('child_process').exec;
 
 ngrok.connect(8000, (err, url) => {
   if (err) {
-    process.exit(1);
+    // process.exit(1);
   } else {
     fs.writeFile('.meta.webhooks.url', url);
-    // exec(`echo "${url}" | xclip -i -selection clipboard`);     // Copy URL to clipboard.
-    exec(`xdg-open "${url}"`);                                 // Open URL in default browser.
+
+    // Copy URL to clipboard.
+    // exec(`echo "${url}" | xclip -i -selection clipboard`);
+
+    // Open URL in default browser.
+    exec(`xdg-open "${url}"`);
+
     nodemon(`-x 'NGROK_URL=${url} node' ./server.js`);
   }
 });
